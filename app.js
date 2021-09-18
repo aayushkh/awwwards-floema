abbc14e86f4a996d41135c56e160b06c5fbf93ab
Aayush Khanna <aayushkhanna@gmail.com>
2021-09-18 10:48:14 -0700

    :fire:

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World! Potato!');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')   // Sets the Template Engine
+
+app.get('/', async (req, res) => {
+  res.render('pages/home')
+})
+
+app.get('/about', async (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/collection', (req, res) => {
+  res.render('pages/collection')
+})
+
+app.get('/detail/:uid', async (req, res) => {
+  res.render('pages/detail')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
